7fb5371d7b69f04cad13caab209a3f77aa59307a
Basineni007 <153413320+Basineni007@users.noreply.github.com>
2024-09-16 11:43:37 +0530

    Update App.java

--- App.java
+++ App.java
@@ -5,34 +5,26 @@
 import java.util.*;
 public class App    
 {  
-    public static void printPattern(int n)
+     public static void printPattern(int n)
     {
         int i, j;
-
         // outer loop to handle number of rows
-        for (i = 1; i <= n; i++) {
-            // inner loop to print the spaces
-            for (j = 1; j <= 2 * (n - i); j++) {
-                System.out.print(" ");
+        for (i = 0; i < n; i++) {
+            //  inner loop to handle number of columns
+            for (j = 0; j < n; j++) {
+                // star will print only when  it is in first
+                // row or last row or first column or last
+                // column
+                if (i == 0 || j == 0 || i == n - 1
+                    || j == n - 1) {
+                    System.out.print("*");
+                }
+                // otherwise print space only.
+                else {
+                    System.out.print(" ");
+                }
             }
-
-            // inner loop to print the first part
-            for (j = i; j >= 1; j--) {
-                System.out.print(j + " ");
-            }
-
-            // inner loop to print the second part
-            for (j = 2; j <= i; j++) {
-                System.out.print(j + " ");
-            }
-
-            // printing new line for each row
             System.out.println();
         }
     }
-    public static void main( String[] args )
-    {
-        int n = 6;
-        printPattern(n);
-    }
-}
+    
