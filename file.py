284acc2da5bb77d6ccf02f1dfc82e44b6f09ab5d
Basineni007 <153413320+Basineni007@users.noreply.github.com>
2024-09-18 11:25:01 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,1 +1,10 @@
-print("hello big wide world")
+# Function to print inverted half pyramid pattern
+def inverted_half_pyramid(n):
+    for i in range(n, 0, -1):
+        for j in range(1, i + 1):
+            print("* ", end="")
+        print("\r")
+
+# Example: Inverted Half Pyramid with n = 5
+n = 5
+inverted_half_pyramid(n)
